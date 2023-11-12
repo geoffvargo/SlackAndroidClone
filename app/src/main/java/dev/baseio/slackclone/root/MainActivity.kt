bbb499daedcc00417be9cef653b005837e9b0a2b
geoffvargo <geoff.vargo@stonybrook.edu>
2023-11-11 23:10:09 -0500

    FORK COMMIT

--- app/src/main/java/dev/baseio/slackclone/root/MainActivity.kt
+++ app/src/main/java/dev/baseio/slackclone/root/MainActivity.kt
@@ -1,52 +1,44 @@
 package dev.baseio.slackclone.root
 
-import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.LaunchedEffect
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import dagger.hilt.android.AndroidEntryPoint
+import android.os.*
+import androidx.activity.compose.*
+import androidx.appcompat.app.*
+import androidx.compose.runtime.*
+import androidx.core.view.*
+import androidx.navigation.compose.*
+import dagger.hilt.android.*
 import dev.baseio.slackclone.R
+import dev.baseio.slackclone.navigator.*
 import dev.baseio.slackclone.navigator.ComposeNavigator
-import dev.baseio.slackclone.navigator.SlackRoute
-import dev.baseio.slackclone.uidashboard.nav.dashboardNavigation
-import dev.baseio.slackclone.uionboarding.nav.onboardingNavigation
-import javax.inject.Inject
+import dev.baseio.slackclone.uidashboard.nav.*
+import dev.baseio.slackclone.uionboarding.nav.*
+import javax.inject.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-  @Inject
-  lateinit var composeNavigator: ComposeNavigator
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    setTheme(R.style.Theme_SlackJetpackCompose)
-    super.onCreate(savedInstanceState)
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-
-    setContent {
-      val navController = rememberNavController()
-
-      LaunchedEffect(Unit) {
-        composeNavigator.handleNavigationCommands(navController)
-      }
-
-      NavHost(
-        navController = navController,
-        startDestination = SlackRoute.OnBoarding.name,
-      ) {
-        onboardingNavigation(
-          composeNavigator = composeNavigator,
-        )
-        dashboardNavigation(
-          composeNavigator = composeNavigator
-        )
-      }
-
-
-    }
-  }
+	
+	@Inject
+	lateinit var composeNavigator: ComposeNavigator
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		setTheme(R.style.Theme_SlackJetpackCompose)
+		super.onCreate(savedInstanceState)
+		WindowCompat.setDecorFitsSystemWindows(window, false)
+		
+		setContent {
+			val navController = rememberNavController()
+			
+			LaunchedEffect(Unit) {
+				composeNavigator.handleNavigationCommands(navController)
+			}
+			
+			NavHost(navController = navController,
+			        startDestination = SlackRoute.OnBoarding.name) {
+				onboardingNavigation(composeNavigator = composeNavigator)
+				dashboardNavigation(composeNavigator = composeNavigator)
+			}
+			
+			
+		}
+	}
 }
--- ui-dashboard/src/main/java/dev/baseio/slackclone/uidashboard/nav/dashboardNavigation.kt
+++ ui-dashboard/src/main/java/dev/baseio/slackclone/uidashboard/nav/dashboardNavigation.kt
@@ -1,38 +1,31 @@
 package dev.baseio.slackclone.uidashboard.nav
 
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.compose.composable
-import androidx.navigation.navigation
+import androidx.navigation.*
+import androidx.navigation.compose.*
+import dev.baseio.slackclone.navigator.*
 import dev.baseio.slackclone.navigator.ComposeNavigator
-import dev.baseio.slackclone.navigator.SlackRoute
-import dev.baseio.slackclone.navigator.SlackScreen
-import dev.baseio.slackclone.uichannels.createsearch.CreateNewChannelUI
-import dev.baseio.slackclone.uichannels.createsearch.SearchCreateChannelUI
-import dev.baseio.slackclone.uichat.newchat.NewChatThreadScreen
-import dev.baseio.slackclone.uidashboard.compose.DashboardUI
-import dev.baseio.slackclone.uidashboard.home.preferences.PreferencesUI
+import dev.baseio.slackclone.uichannels.createsearch.*
+import dev.baseio.slackclone.uichat.newchat.*
+import dev.baseio.slackclone.uidashboard.compose.*
+import dev.baseio.slackclone.uidashboard.home.preferences.*
 
-fun NavGraphBuilder.dashboardNavigation(
-  composeNavigator: ComposeNavigator,
-) {
-  navigation(
-    startDestination = SlackScreen.Dashboard.name,
-    route = SlackRoute.Dashboard.name
-  ) {
-    composable(SlackScreen.Dashboard.name) {
-      DashboardUI(composeNavigator)
-    }
-    composable(SlackScreen.CreateChannelsScreen.name) {
-      SearchCreateChannelUI(composeNavigator = composeNavigator)
-    }
-    composable(SlackScreen.CreateNewChannel.name) {
-      CreateNewChannelUI(composeNavigator)
-    }
-    composable(SlackScreen.CreateNewDM.name) {
-      NewChatThreadScreen(composeNavigator)
-    }
-    composable(SlackScreen.SlackPreferences.name) {
-      PreferencesUI(composeNavigator = composeNavigator)
-    }
-  }
+fun NavGraphBuilder.dashboardNavigation(composeNavigator: ComposeNavigator) {
+	navigation(startDestination = SlackScreen.Dashboard.name,
+	           route = SlackRoute.Dashboard.name) {
+		composable(SlackScreen.Dashboard.name) {
+			DashboardUI(composeNavigator)
+		}
+		composable(SlackScreen.CreateChannelsScreen.name) {
+			SearchCreateChannelUI(composeNavigator = composeNavigator)
+		}
+		composable(SlackScreen.CreateNewChannel.name) {
+			CreateNewChannelUI(composeNavigator)
+		}
+		composable(SlackScreen.CreateNewDM.name) {
+			NewChatThreadScreen(composeNavigator)
+		}
+		composable(SlackScreen.SlackPreferences.name) {
+			PreferencesUI(composeNavigator = composeNavigator)
+		}
+	}
 }
